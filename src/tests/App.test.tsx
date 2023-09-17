95c05f9a8da4cf934609447bd77c8b25af1e4221
Ron <lavitr01051977@gmail.com>
2023-09-17 05:38:24 +0200

    feat: additional test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -7,7 +7,7 @@ test("renders App", () => {
       data: {
         dataNext: [],
         dataPrev: [],
-        status: "idle",
+        status: "complete",
         error: "",
       },
       switch: { isPrevJobs: true },
@@ -15,4 +15,34 @@ test("renders App", () => {
   });
   expect(screen.getByText("All my Cleanings")).toBeInTheDocument();
   expect(screen.getAllByRole("button")).toHaveLength(2);
+});
+
+test("App should display loader when loading data", () => {
+  const screen = renderWithProviders(<App />, {
+    preloadedState: {
+      data: {
+        dataNext: [],
+        dataPrev: [],
+        status: "loading",
+        error: "",
+      },
+      switch: { isPrevJobs: true },
+    },
+  });
+  expect(document.querySelector(".loader-wrapper")).toBeInTheDocument();
+});
+
+test("App should display error when fetching is failed", () => {
+  const screen = renderWithProviders(<App />, {
+    preloadedState: {
+      data: {
+        dataNext: [],
+        dataPrev: [],
+        status: "complete",
+        error: "Error",
+      },
+      switch: { isPrevJobs: true },
+    },
+  });
+  expect(screen.getByText("Something went wrong:")).toBeInTheDocument();
 });
